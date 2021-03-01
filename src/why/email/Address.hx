365a3bc8cb75a58cf667850ac2ba1059d2d74839
Kevin Leung <kevinresol@gmail.com>
2021-03-01 11:38:31 +0800

    Validate email address format

--- src/why/email/Address.hx
+++ src/why/email/Address.hx
@@ -1,29 +1,37 @@
 package why.email;
 
 using StringTools;
+using tink.CoreApi;
 
 @:forward
 abstract Address(AddressBase) from AddressBase to AddressBase {
 	
-	// TODO: validate email address
-	// static var REGEX = ~/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+	static var REGEX = ~/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+	
+	public static function parse(v:String):Outcome<Address, Error> {
+		return Error.catchExceptions(fromString.bind(v));
+	}
 	
 	@:from
-	public static function parse(v:String):Address {
+	static function fromString(v:String):Address {
 		var re = ~/^(.*)\s?<(.*)>$/;
 		return if(re.match(v)) {
 			var name = re.matched(1).trim();
 			if(name.charCodeAt(0) == '"'.code && name.charCodeAt(name.length - 1) == '"'.code)
 				name = name.substr(1, name.length - 2);
-			{name: name, address: re.matched(2)}
+			{name: name, address: parseAddress(re.matched(2))}
 		} else {
-			{address: v}
+			{address: parseAddress(v)}
 		}
 	}
 	
 	@:to
 	public function toString():String {
 		return if(this.name == null) this.address else '"${this.name}" <${this.address}>';
+	}
+		
+	static function parseAddress(v:String) {
+		return REGEX.match(v) ? v : throw 'Invalid email address';
 	}
 }
 
